2a08837541ec64590091bc15997511581887f522
unknown <aimable.kwizera14@gmail.com>
2024-03-11 10:15:51 +0200

    It is initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+export const calc = {
+  add(num1, num2, num3, num4) {
+    return num1 + num2 + num3 + num4;
+  },
+
+  sub(num1, num2) {
+    return num1 - num2;
+  },
+
+  mul(num1, num2) {
+    return num1 + num2;
+  },
+};
